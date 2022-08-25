a91c2a70a1973ba8dc8779f606e66b4dcec60be3
Matt Waelder <mattwaelder@gmail.com>
2022-08-24 17:25:44 -0700

    within app.jsx: added xml reference to ratings and reviews component instantiated a ratings and reviews component with a simple greeting text added some notes on plans for file substructure for ratings and reviews component

---
+++ client/src/components/RatingsAndReviews/RatingsAndReviewsMain.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+class RatingsAndReviewsMain extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>HELLO RATINGS... AND REVIEWS!</div>;
+  }
+}
+
+export default RatingsAndReviewsMain;
+
+/*      NOTES
+
+  will need to know what product is currently seleted (props?)
+
+  sub-components of ratings and reviews component
+
+  SEARCH
+
+  LIST
+    > LIST ITEM
+
+  RATINGS BD
+    >STAR VALUE
+    >PRODUCT BD
+      ??> PRODUCT BD CATEGORIES
+
+*/
