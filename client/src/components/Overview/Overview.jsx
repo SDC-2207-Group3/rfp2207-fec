11e49d15b757a9031d8046d5172de6eb42af6f30
Ivan Luk <luk.ivan.mech@gmail.com>
2022-08-24 20:30:18 -0700

    Created Overview component

---
+++ client/src/components/Overview/Overview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Overview from React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Overview
