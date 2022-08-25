8aa26935c893378deb8e8cf1e3e623c95565bed0
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-08-25 10:06:12 -0700

    save skeleton files

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,6 +1,8 @@
 var RelatedItems = (props) => {
   return (
-    <div><h2>Related Items and Comparison Here =D</h2></div>
+    <div>
+      <h2>Related Items and Comparison Here =D</h2>
+    </div>
   )
 }
 
