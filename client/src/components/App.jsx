d73ad7a4b3ecfdeac5c3314a4b9ad28c0369a3c4
Ivan Luk <luk.ivan.mech@gmail.com>
2022-08-24 20:36:48 -0700

    Overview progress

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Overview from "./Overview/Overview.jsx";
 import RatingsAndReviews from "./RatingsAndReviews/RatingsAndReviewsMain.jsx";
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <div>
         <h1> hello world </h1>
+        <Overview />
         <div>
           <RatingsAndReviews />
         </div>
--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 
-class Overview from React.Component {
+
+class Overview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
