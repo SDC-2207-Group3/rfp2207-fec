81f2364b935c8e7ffaa43a79cfd447453c08f1bd
Matt Waelder <mattwaelder@gmail.com>
2022-08-26 12:42:56 -0700

    refactored reviewsList component to use useState/useEffect hooks, added additional support for conditional rendering which is particularly nice for the moments before api requests are available.

--- client/src/components/RatingsAndReviews/SubComponents/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/SubComponents/ReviewsList.jsx
@@ -1,26 +1,48 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import ReviewItem from "./ReviewItem.jsx";
 
-class ReviewsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    // let reviewArr = [];
-    // this.props.reviews.forEach((el) => {
-    //   reviewArr.push(<ReviewItem review={el} />);
-    // });
-    return (
-      <div id="RR_reviews-list">
-        <p>Reviews List</p>
-        <div>
-          {this.props.reviews.map((el, i) => {
-            return <ReviewItem key={i} review={el} />;
-          })}
-        </div>
+// class ReviewsList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     // let reviewArr = [];
+//     // this.props.reviews.forEach((el) => {
+//     //   reviewArr.push(<ReviewItem review={el} />);
+//     // });
+//     return (
+//       <div id="RR_reviews-list">
+//         <p>Reviews List</p>
+//         <div>
+//           {this.props.reviews.map((el, i) => {
+//             return <ReviewItem key={i} review={el} />;
+//           })}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+//props = reviews, id
+let ReviewsList = ({ reviews, id }) => {
+  const [reviewsList, setReviewsList] = useState({});
+  useEffect(() => {
+    setReviewsList(reviews);
+  }, [reviews]);
+
+  return (
+    <div id="RR_reviews-list">
+      <p>Reviews List</p>
+      <div>
+        {Object.keys(reviewsList).length
+          ? reviewsList.map((el, i) => {
+              return <ReviewItem key={i} review={el} />;
+            })
+          : null}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ReviewsList;
