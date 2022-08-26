c922559fc0978f825d86194ad3d5bdc50f5f0409
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-08-26 00:12:55 -0700

    made ratings stars and installed react-feather

--- client/src/components/RelatedItems/ProductCard.jsx
+++ client/src/components/RelatedItems/ProductCard.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Stars from './Stars.jsx';
 
 var ProductCard = (props) => {
-
+  // const rating = React.useContext(DataContext);
+  // console.log('--use context--', rating);
   return (
     <div>
       <p>this is a product card</p>
+      <Stars />
     </div>
   )
 }
