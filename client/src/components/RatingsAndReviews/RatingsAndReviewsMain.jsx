d13266faaad874846b93f562abc8f5178607527b
Matt Waelder <mattwaelder@gmail.com>
2022-08-29 12:35:45 -0700

    hooks working on all needed components of reviews / ratings adjusted css file to fix box-sizing and root width -as discussed

--- client/src/components/RatingsAndReviews/RatingsAndReviewsMain.jsx
+++ client/src/components/RatingsAndReviews/RatingsAndReviewsMain.jsx
@@ -7,99 +7,9 @@ import ReviewsList from "./SubComponents/ReviewsList.jsx";
 
 import { useState, useEffect, useReducer } from "react";
 
-// class RatingsAndReviewsMain extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       id: this.props.id || "65631",
-//       selectedItem: {},
-//       sortBy: "newest",
-//       displayedReviews: 2,
-//       reviews: [],
-//       meta: {},
-//       reviewStats: {},
-//     };
-
-//     this.getAvgReviewValue = this.getAvgReviewValue.bind(this);
-//   }
-
-//   ////////////////////////////////////////////////////////////
-
-//   getAvgReviewValue(meta) {
-//     // if (Object.keys(this.props.meta).length) {
-//     let statsObj = {};
-//     statsObj.starTotal = 0;
-//     statsObj.voteTotal = 0;
-//     for (let [star, count] of Object.entries(meta.ratings)) {
-//       statsObj.starTotal += star * count;
-//       statsObj.voteTotal += Number(count);
-//     }
-//     return statsObj;
-//   }
-
-//   componentDidMount() {
-//     //params = page, count, sort, product_id
-//     //get reviews for current product
-//     axios
-//       .get(`${ATELIER_API}/reviews`, {
-//         params: {
-//           product_id: `${this.state.id}`,
-//           sort: `${this.state.sortBy}`,
-//           count: `${this.state.displayedReviews}`,
-//         },
-//         headers: { Authorization: process.env.KEY },
-//       })
-//       //res.data.results = arr of reviews
-//       .then((res) => {
-//         //update state
-//         this.setState({ reviews: res.data.results });
-//       })
-//       .then(() => {
-//         // get meta data for current product
-//         return axios.get(`${ATELIER_API}/reviews/meta`, {
-//           params: { product_id: `${this.state.id}` },
-//           headers: { Authorization: process.env.KEY },
-//         });
-//       })
-//       .then((res) => {
-//         //create meta info to pass to sub component
-//         let reviewStatsObj = this.getAvgReviewValue(res.data);
-
-//         //update state once more
-//         this.setState({
-//           meta: res.data,
-//           reviewStats: reviewStatsObj,
-//         });
-//       })
-//       .catch((err) => console.log("failed to fetch", err));
-//   }
-
-//   ////////////////////////////////////////////////////////////
-
-//   render() {
-//     return (
-//       <section id="section_rr">
-//         <h2>Ratings and Reviews</h2>
-//         <div id="RR_bd-sort-list-container">
-//           <RatingsBreakDown
-//             reviewStats={this.state.reviewStats}
-//             meta={this.state.meta}
-//             id={this.props.id}
-//           />
-//           <div id="RR_sort-list-container">
-//             <Sort sortMethod={this.state.sortBy} id={this.props.id} />
-//             <ReviewsList reviews={this.state.reviews} id={this.props.id} />
-//           </div>
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-/////////////////////  HOOKS  //////////////////////
-
 let initialState = {
-  id: "65631",
-  selectedItem: {},
+  id: "65635",
+  // selectedItem: {},
   sortBy: "newest",
   displayedReviews: 2,
   reviews: [],
@@ -110,22 +20,41 @@ let initialState = {
 let reducer = (state, action) => {
   switch (action.type) {
     case "setId":
-      return { state };
+      return { ...state, id: action.setId };
     case "setSort":
-      return { state };
+      return { ...state, sortBy: action.setSort };
     case "setDisplayedReviews":
-      return { state };
+      return { ...state, displayedReviews: action.setDisplayedReviews };
     case "setReviews":
-      return { state };
+      return { ...state, reviews: action.setReviews };
     case "setMeta":
-      return { state };
+      return { ...state, meta: action.setMeta };
     case "setReviewStats":
-      return { state };
+      return { ...state, reviewStats: action.setReviewStats };
     case "setAll":
-      return { state };
+      return {
+        id: action.setId,
+        sortBy: action.sortBy,
+        displayedReviews: action.displayedReviews,
+        reviews: action.setReviews,
+        meta: action.setMeta,
+        reviewStats: action.setReviewStats,
+      };
     default:
       return state;
   }
+};
+
+let getAvgReviewValue = (meta) => {
+  // if (Object.keys(this.props.meta).length) {
+  let statsObj = {};
+  statsObj.starTotal = 0;
+  statsObj.voteTotal = 0;
+  for (let [star, count] of Object.entries(meta.ratings)) {
+    statsObj.starTotal += star * count;
+    statsObj.voteTotal += Number(count);
+  }
+  return statsObj;
 };
 
 //should make axios request, then instead of setting state
@@ -133,6 +62,52 @@ let reducer = (state, action) => {
 
 let RatingsAndReviewsMain = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  let fetchData = (state) => {
+    axios
+      .get(`${ATELIER_API}/reviews`, {
+        params: {
+          product_id: `${state.id}`,
+          sort: `${state.sortBy}`,
+          count: `${state.displayedReviews}`,
+        },
+        headers: { Authorization: process.env.KEY },
+      })
+      //res.data.results = arr of reviews
+      .then((res) => {
+        console.warn(res);
+        //update state
+        dispatch({
+          type: "setReviews",
+          setReviews: res.data.results,
+        });
+      })
+      .then(() => {
+        console.warn("meta call");
+        // get meta data for current product
+        return axios.get(`${ATELIER_API}/reviews/meta`, {
+          params: { product_id: `${state.id}` },
+          headers: { Authorization: process.env.KEY },
+        });
+      })
+      .then((res) => {
+        let reviewStatsObj = getAvgReviewValue(res.data);
+
+        dispatch({
+          type: "setMeta",
+          setMeta: res.data,
+        });
+        dispatch({
+          type: "setReviewStats",
+          setReviewStats: reviewStatsObj,
+        });
+      })
+      .catch((err) => console.log("failed to fetch", err));
+  };
+
+  useEffect(() => {
+    fetchData(state);
+  }, []);
 
   return (
     <section id="section_rr">
