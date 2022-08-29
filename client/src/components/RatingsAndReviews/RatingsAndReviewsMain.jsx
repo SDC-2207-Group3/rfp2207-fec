d35aacebba5097f00cd37e34697e267efadb3734
Matt Waelder <mattwaelder@gmail.com>
2022-08-29 16:28:13 -0700

    added functionality to add reviews 2 at a time to a scrollable reviews list once there are no more reviews, the buttton to add more goes away still some bugs to fix, though.

--- client/src/components/RatingsAndReviews/RatingsAndReviewsMain.jsx
+++ client/src/components/RatingsAndReviews/RatingsAndReviewsMain.jsx
@@ -4,18 +4,7 @@ import ATELIER_API from "./utilities/utilities.js";
 import Sort from "./SubComponents/Sort.jsx";
 import RatingsBreakDown from "./SubComponents/RatingsBreakDown.jsx";
 import ReviewsList from "./SubComponents/ReviewsList.jsx";
-
 import { useState, useEffect, useReducer } from "react";
-
-let initialState = {
-  id: "65635",
-  // selectedItem: {},
-  sortBy: "newest",
-  displayedReviews: 2,
-  reviews: [],
-  meta: {},
-  reviewStats: {},
-};
 
 let reducer = (state, action) => {
   switch (action.type) {
@@ -31,22 +20,23 @@ let reducer = (state, action) => {
       return { ...state, meta: action.setMeta };
     case "setReviewStats":
       return { ...state, reviewStats: action.setReviewStats };
-    case "setAll":
-      return {
-        id: action.setId,
-        sortBy: action.sortBy,
-        displayedReviews: action.displayedReviews,
-        reviews: action.setReviews,
-        meta: action.setMeta,
-        reviewStats: action.setReviewStats,
-      };
+    case "setShowMoreBtn":
+      return { ...state, showMoreBtn: action.setShowMoreBtn };
+    // case "setAll":
+    //   return {
+    //     id: action.setId,
+    //     sortBy: action.sortBy,
+    //     displayedReviews: action.displayedReviews,
+    //     reviews: action.setReviews,
+    //     meta: action.setMeta,
+    //     reviewStats: action.setReviewStats,
+    //   };
     default:
       return state;
   }
 };
 
 let getAvgReviewValue = (meta) => {
-  // if (Object.keys(this.props.meta).length) {
   let statsObj = {};
   statsObj.starTotal = 0;
   statsObj.voteTotal = 0;
@@ -61,7 +51,56 @@ let getAvgReviewValue = (meta) => {
 //call reducer to set state for us
 
 let RatingsAndReviewsMain = (props) => {
+  let initialState = {
+    id: props.id,
+    // selectedItem: {},
+    sortBy: "newest",
+    displayedReviews: 2,
+    reviews: [],
+    meta: {},
+    reviewStats: {},
+    showMoreBtn: true,
+  };
+
+  //may want to pass dispatch down as props or something?
+
+  //display with x reviews
+  //each time "show more is clicked"
+  //display x+2 reviews
+  //if x+2 reviews count = reviews count remove show more btn
+
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  let showMoreReviews = () => {
+    return axios
+      .get(`${ATELIER_API}/reviews`, {
+        params: {
+          product_id: `${state.id}`,
+          sort: `${state.sortBy}`,
+          count: `${state.displayedReviews + 2}`,
+        },
+        headers: { Authorization: process.env.KEY },
+      })
+      .then((res) => {
+        if (res.data.count > res.data.results.length) {
+          dispatch({
+            type: "setShowMoreBtn",
+            setShowMoreBtn: false,
+          });
+        } else {
+          dispatch({
+            type: "setReviews",
+            setReviews: res.data.results,
+          });
+          dispatch({
+            type: "setDisplayedReviews",
+            setDisplayedReviews: state.displayedReviews + 2,
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+    console.log(state);
+  };
 
   let fetchData = (state) => {
     axios
@@ -120,7 +159,12 @@ let RatingsAndReviewsMain = (props) => {
         />
         <div id="RR_sort-list-container">
           <Sort sortMethod={state.sortBy} id={props.id} />
-          <ReviewsList reviews={state.reviews} id={props.id} />
+          <ReviewsList
+            showMoreBtn={state.showMoreBtn}
+            reviews={state.reviews}
+            id={props.id}
+            showMoreReviews={showMoreReviews}
+          />
         </div>
       </div>
     </section>
--- client/src/components/RatingsAndReviews/SubComponents/ReviewsList.jsx
+++ client/src/components/RatingsAndReviews/SubComponents/ReviewsList.jsx
@@ -2,31 +2,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import ReviewItem from "./ReviewItem.jsx";
 
-// class ReviewsList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-//   render() {
-//     // let reviewArr = [];
-//     // this.props.reviews.forEach((el) => {
-//     //   reviewArr.push(<ReviewItem review={el} />);
-//     // });
-//     return (
-//       <div id="RR_reviews-list">
-//         <p>Reviews List</p>
-//         <div>
-//           {this.props.reviews.map((el, i) => {
-//             return <ReviewItem key={i} review={el} />;
-//           })}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+//props boolean, arr, str, fn
+let ReviewsList = ({ showMoreBtn, reviews, id, showMoreReviews }) => {
+  const [reviewsList, setReviewsList] = useState(reviews);
 
-//props = reviews, id
-let ReviewsList = ({ reviews, id }) => {
-  const [reviewsList, setReviewsList] = useState({});
   useEffect(() => {
     setReviewsList(reviews);
   }, [reviews]);
@@ -35,12 +14,13 @@ let ReviewsList = ({ reviews, id }) => {
     <div id="RR_reviews-list">
       <p>Reviews List</p>
       <div>
-        {Object.keys(reviewsList).length
-          ? reviewsList.map((el, i) => {
-              return <ReviewItem key={i} review={el} />;
-            })
-          : null}
+        {reviewsList.map((el, i) => {
+          return <ReviewItem key={i} review={el} />;
+        })}
       </div>
+      {showMoreBtn ? (
+        <button onClick={() => showMoreReviews()}>show more</button>
+      ) : null}
     </div>
   );
 };
