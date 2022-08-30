b245a882f1ce2d9b2de0aeb579959d6632c88b91
Matt Waelder <mattwaelder@gmail.com>
2022-08-29 17:52:15 -0700

    added props to app.jsx for my subcomponent

--- client/src/components/RatingsAndReviews/SubComponents/RatingsBreakDown.jsx
+++ client/src/components/RatingsAndReviews/SubComponents/RatingsBreakDown.jsx
@@ -1,51 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-// class RatingsBreakDown extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       avg: 0,
-//       meta: this.props.meta,
-//     };
-//   }
-
-//   ////////////////////////////////////////////////////////////
-
-//   ////////////////////////////////////////////////////////////
-
-//   render() {
-//     return (
-//       <div id="RR_break-down-container">
-//         <p>Ratings BreakDown</p>
-//         <h2 id="RR_star-avg">
-//           {this.props.reviewStats.starTotal
-//             ? (
-//                 this.props.reviewStats.starTotal /
-//                 this.props.reviewStats.voteTotal
-//               ).toFixed(1)
-//             : null}{" "}
-//           stars
-//         </h2>
-//         <div className="RR_ratings-bd-list">
-//           {this.props.meta.ratings
-//             ? [5, 4, 3, 2, 1].map((starNum, i) => {
-//                 return (
-//                   <div id="RR_ratings-bd-count" key={starNum}>
-//                     {starNum}:{" "}
-//                     {Object.values(this.props.meta.ratings)[starNum - 1]}
-//                   </div>
-//                 );
-//               })
-//             : null}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-/////////////////////////HHOOOOOOOOKKKKKSSSSS
-
 //props = reviewStats, meta, id
 let RatingsBreakDown = (props) => {
   // state, fn to edit state = hook (initial val)
