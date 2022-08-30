b6b040d1a6b4078119321b3b7a0306727d9e8864
Matt Waelder <mattwaelder@gmail.com>
2022-08-30 12:04:48 -0700

    truncated review content greater than specified max length for each product review added a "show more" button which reveals that truncated content and vanishes upon click began work on displaying user images

--- client/src/components/RatingsAndReviews/SubComponents/ReviewItem.jsx
+++ client/src/components/RatingsAndReviews/SubComponents/ReviewItem.jsx
@@ -1,12 +1,40 @@
 import React from "react";
+import { useState } from "react";
 
 var ReviewItem = (props) => {
+  const [renderBody, setRenderBody] = useState({ render: false });
+
+  console.log(props.review);
+
   return (
     <li className="RR_list-item">
       <p>username: {props.review.reviewer_name}</p>
       <span>posted: {props.review.date}</span>
-      <h4>{props.review.summary}</h4>
-      <p className="RR_review-body">{props.review.body}</p>
+      <h4>
+        {props.review.summary.length <= 60
+          ? props.review.summary
+          : props.review.summary.slice(0, 59) + "..."}
+      </h4>
+      <p className="RR_review-body">
+        {props.review.body.length > 250 && !renderBody.render
+          ? props.review.body.slice(0, 250) + "..."
+          : props.review.body}
+      </p>
+      <span
+        onClick={() => setRenderBody({ render: true })}
+        id="RR_review-show-more"
+      >
+        {props.review.body.length > 250 && !renderBody.render ? (
+          <button>show more</button>
+        ) : null}
+      </span>
+      <div>
+        {props.review.photos.length
+          ? props.review.photos.map((photo) => {
+              return <image alt="user submitted image" src={`${photo.url}`} />;
+            })
+          : null}
+      </div>
     </li>
   );
 };
