d34023b57523b61d8ba98c7031039a37173020e0
Jessie Zhao <jcz11213@gmail.com>
2022-08-29 18:44:35 -0700

    added display more answers button to qaEntry component

--- client/src/components/QuestionsAndAnswers/QAEntry.jsx
+++ client/src/components/QuestionsAndAnswers/QAEntry.jsx
@@ -8,6 +8,9 @@ function QAEntry({question, onToggle, active}) {
   // // initializing state for question and answer modals
   // const [openModal, setOpenModal] = useState(false);
 
+  // state hook for showing more answers
+  const [answersCount, incrementAnswers] = useState(2)
+
   return(
     <li className={`qa-accordion-item ${active ? "active" : ""}`}>
       <button className="question-item-button" onClick={onToggle}>
@@ -17,10 +20,20 @@ function QAEntry({question, onToggle, active}) {
       <div className={`answer-wrapper ${active ? "open" : ""}`}>
         <div className="answer-item">
           {
-            Object.keys(question.answers).slice(0,2).map((answer_id, index) =>
+            Object.keys(question.answers).slice(0, answersCount).map((answer_id, index) =>
               <AnswerItem answer={question.answers[answer_id]} />
             )
           }
+          {
+            Object.keys(question.answers).slice(answersCount).length > 0
+            &&
+            <button className="qa-moreAnswers"
+            onClick={() => incrementAnswers(answersCount + 2)}
+            >
+              Load More Answers
+            </button>
+          }
+
         </div>
 
       </div>
